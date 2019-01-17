cb58451c3d0869e7d3a44921029dca527c59b4a8
Ô Duy <33828921+oduy@users.noreply.github.com>
2019-01-18 01:04:43 +0700

    completed structure example

--- Assets/Script/DelayTime.cs
+++ Assets/Script/DelayTime.cs
@@ -6,46 +6,6 @@ using UnityEngine.UI;
 
 public class DelayTime : MonoBehaviour
 {
-    public Text text;
-    public int Time = 10;
-    public int numscene = 1;
-
-    void Start()
-    {
-        if (text != null)
-            text.text = Time.ToString();
-    }
-
-    void Update()
-    {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Manager.CurrentState == Manager.State.MENU)
-            {
-                StartCoroutine(Timedelay());
-                Manager.CurrentState = Manager.State.GAMEPLAY;
-            }
-            Time = 10;
-            if (text != null)
-                text.text = Time.ToString();
-        }
-        if (Time == 0)
-        {
-            //  Manager.CurrentState = Manager.State.START;
-        }
-    }
-
-
-
-    IEnumerator Timedelay()
-    {
-        yield return new WaitForSeconds(1);
-        Time--;
-        text.text = Time.ToString();
-        if (Time > 0)
-        {
-            StartCoroutine(Timedelay());
-        }
-    }
+ 
+    
 }
--- Assets/Script/Manager.cs
+++ Assets/Script/Manager.cs
@@ -6,39 +6,86 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
-    
+    public Text text;
+    public GameObject btnPlay;
+    public GameObject btnReplay;
+    public int Time = 5;
+    public int numscene = 1;
 
-    public enum State {MENU, GAMEPLAY, NULL};
-    public static  State CurrentState;
-
+    public enum State {MENU, GAMEPLAY, GAMEOVER, NULL};
+    private State CurrentState = State.MENU;
 
     void Start(){
-        CurrentState= State.MENU;
-    }
+        btnReplay.SetActive(false);
+    }    
 
+    private bool runOneTime = false;
     void Update()
     {
         switch(CurrentState)
         {
             case State.MENU:{
-               // SceneManager.LoadScene(0);
-               
+                btnPlay.SetActive(true);
                 break;
             }
             case State.GAMEPLAY:{
-               // SceneManager.LoadScene(1);
-                print("run gameplay");
+                funcGameplay();
                 break;
             }
-            case State.NULL:{
 
+            case State.GAMEOVER:{
+                btnReplay.SetActive(true);
                 break;
             }
         }
         print(CurrentState);
     }
 
+    
 
+    IEnumerator Timedelay()
+    {
+        yield return new WaitForSeconds(1f);
+        Time--;
+        text.text = Time.ToString();
+        if (Time > 0)
+        {
+            StartCoroutine(Timedelay());
+        }
+    }
 
+    private void funcGameplay(){
+        if(!runOneTime){
+            Time = 5;
+            text.text = Time.ToString();
+            btnReplay.SetActive(false);
+            btnPlay.SetActive(false);
+            StartCoroutine(Timedelay());  
+            runOneTime = true;  
+        }
+        if (Input.GetMouseButtonDown(0))
+        {   
+            Time = 5;
+            if (text != null)
+                text.text = Time.ToString();
+        }
+        if (Time == 0)
+        {
+            CurrentState = State.GAMEOVER;
+        }
+    }
+
+    public void funcMenu(){
+        runOneTime = false;
+        CurrentState = State.GAMEPLAY;
+        btnReplay.SetActive(false);
+        
+    }
+
+    public void funcGameover(){
+        CurrentState = State.MENU;
+        btnReplay.SetActive(false);
+        
+    }
     
 }
