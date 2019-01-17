b6440d54cb98a234fbcaba2f57159d52b270390f
Ô Duy <33828921+oduy@users.noreply.github.com>
2019-01-17 19:49:14 +0700

    make scene: start, time delay

---
+++ Assets/Script/DelayTime.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DelayTime : MonoBehaviour
+{
+    public Text text;
+    public int Time = 10;
+    public int numscene = 1;
+
+    void Start(){
+        if(text != null)
+            text.text = Time.ToString();
+        StartCoroutine(Timedelay());
+
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Time = 10;
+            if(text != null)
+            text.text = Time.ToString();
+        }
+        if(Time == 0)
+        {
+            SceneManager.LoadScene(numscene);
+        }
+    }
+
+
+
+    IEnumerator Timedelay()
+    {
+        yield return new WaitForSeconds(1);
+        Time--;
+        text.text = Time.ToString();
+        if(Time > 0){
+            StartCoroutine(Timedelay());
+        }
+    }
+}
---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public float time = 2f;
+    public int numscene = 1;
+
+    void Start()
+    {
+        StartCoroutine(Timedelay(time));
+    }
+    
+    IEnumerator Timedelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(numscene);
+    }
+}
